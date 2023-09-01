1218c035bbdf09ee9d4389fecb21bb99ba32b348
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-31 22:46:10 -0300

    .

---
+++ React/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/header/header"
+import Banner from "./components/banner/banner"
+import Mortes from "./components/mortes/mortes"
+import Epidemia from "./components/epidemia/epidemia"
+import Contato from "./components/contato/contato"
+import Footer from "./components/footer/footer"
+
+export default function App(){
+
+  return (
+    <>
+      <Header />
+      <Banner/>
+      <Mortes/>
+      <Epidemia/>
+      <Contato/>
+      <Footer/>
+    </>
+  )
+}
+
